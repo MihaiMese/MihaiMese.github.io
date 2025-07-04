b63b225303dd63982083c5089bbd7f34353634ab
mihaimesesan <Mihai.Mesesan@fanduel.com>
2025-07-04 11:42:13 +0300

    Mavenizing project

---
+++ src/main/java/io/github/mihaimese/Runner.java
@@ -0,0 +1,8 @@
+package io.github.mihaimese;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        System.out.println("It works!");
+    }
+}
---
+++ src/test/java/io/github/mihaimese/MyTest.java
@@ -0,0 +1,12 @@
+package io.github.mihaimese;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MyTest {
+
+    @Test
+    void testMe() {
+        Assertions.assertTrue(!"Hello".isEmpty());
+    }
+}
